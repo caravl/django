2413dbecbd964a808d8d8b9fc92ea4397ffc7259
Cara Lang <clang115@icloud.com>
2018-12-31 15:23:09 -0500

    reset

--- tripplanner-routing/server/models/itinerary.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require("sequelize");
-const db = require("./_db");
-
-const Itinerary = db.define("itinerary", {
-});
-
-module.exports = Itinerary;
--- wikistacks_live_review/models/index.js
+++
@@ -1,7 +0,0 @@
-//
-const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack');
-
-// pass in
-const Page = require('./page');
-module.exports = db;
